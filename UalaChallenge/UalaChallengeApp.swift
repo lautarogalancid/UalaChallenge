5edda40615754cd1c1e7f59928e1037d53f48da9
Lautaro Emanuel Galán Cid <36479078+lautarogalancid@users.noreply.github.com>
2024-11-21 10:33:51 -0300

    Initial Commit

---
+++ UalaChallenge/UalaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaChallengeApp.swift
+//  UalaChallenge
+//
+//  Created by Lautaro Emanuel Galan Cid on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct UalaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
